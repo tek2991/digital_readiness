3627b85ce4f580221e441a993b7922944d9087d9
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-02-12 12:21:15 +0530

    Home page drafted

--- resources/views/layouts/public-footer.blade.php
+++ resources/views/layouts/public-footer.blade.php
@@ -2,26 +2,50 @@
     <div class="p-4 bg-intelblue2 mt-12">
         <div class="container mx-auto">
             <footer>
-                <div class="md:grid md:grid-cols-3">
+                <div class="my-8">
+                    <img src="{{ asset('images/logo.png') }}" alt="">
+                </div>
+                <div class="md:grid md:grid-cols-3 mb-4">
                     <div>
-                        <img src="{{ asset('images/logo.png') }}" alt="">
-                        <p class="">
+                        <p class="text-xs text-gray-600 font-intellight text-justify">
                             Intel technologies may require enabled hardware, software or
-                            service activation. // No product or component can be absolutely
-                            secure. // Your costs and results may vary. // Performance
-                            varies by use, configuration and other factors. // See our
-                            complete legal Notices and Disclaimers. // Intel is committed to
+                            service activation. No product or component can be absolutely
+                            secure. Your costs and results may vary. Performance
+                            varies by use, configuration and other factors. See our
+                            complete legal <a href="#" class="font-intelmedium text-gray-900">Notices and
+                                Disclaimers</a>. Intel is committed to
                             respecting human rights and avoiding complicity in human rights
-                            abuses. See Intel’s Global Human Rights Principles. Intel’s
+                            abuses. See <a href="#" class="font-intelmedium text-gray-900">Intel’s Global Human
+                                Rights Principles</a>. Intel’s
                             products and software are intended only to be used in
                             applications that do not cause or contribute to a violation of
                             an internationally recognised human right.
                         </p>
                     </div>
-                    <div class="md:col-span-2">
-
+                    <div class="md:col-span-2 mt-8 md:mt-0">
+                        <div class="flex justify-between md:justify-around">
+                            <div class="flex flex-col gap-y-4">
+                                <a href="#" class="text-intelblue text-sm hover:underline">For Industry
+                                    Partners</a>
+                                <a href="#" class="text-intelblue text-sm hover:underline">For Institutions</a>
+                                <a href="#" class="text-intelblue text-sm hover:underline">For Students</a>
+                            </div>
+                            <div class="flex flex-col gap-y-4">
+                                <a href="#" class="text-intelblue text-sm hover:underline">About Us</a>
+                                <a href="#" class="text-intelblue text-sm hover:underline">FAQs</a>
+                                <a href="#" class="text-intelblue text-sm hover:underline">Contact Us</a>
+                            </div>
+                            <div class="flex flex-col gap-y-4">
+                                <a href="#" class="text-intelblue text-sm hover:underline">Terms & Conditions</a>
+                                <a href="#" class="text-intelblue text-sm hover:underline">Privacy Policies</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <hr class="border-b border-gray-300 rounded-sm">
+                <p class="pt-6 pb-2">
+                    <a href="#" class="text-gray-600 text-xs font-intellight">© 2023 Intel Simulated Internship. All rights reserved.</a>
+                </p>
             </footer>
         </div>
     </div>
