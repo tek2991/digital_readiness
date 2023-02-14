2b667320eec750a61e432c56016b7460f1954a5d
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-02-14 13:33:52 +0530

    Added first slide

--- resources/views/livewire/lesson/wrapper.blade.php
+++ resources/views/livewire/lesson/wrapper.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div class="mx-auto p-2 bg-black text-white rounded-xl max-w-sm text-center">
+    {{-- <div class="mx-auto p-2 bg-black text-white rounded-xl max-w-sm text-center">
         Lesson ID: {{ $lesson_id }}
-    </div>
+    </div> --}}
     @if ($lesson_id == 1)
         @livewire('lesson.m1l1')
     @endif
